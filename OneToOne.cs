6cda43caded678962b396dce8337243ef11a1fed
USSL <USSL@DESKTOP-REF33GG>
2022-06-16 10:59:43 +0600

    Add project files.

---
+++ OneToOne.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MyHub
+{
+    public class OneToOne : PersistentConnection
+    {
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            
+            return Connection.Broadcast(connectionId+"****"+ request.QueryString["userName"]);
+        }
+
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            int i = data.LastIndexOf("<<<<");
+            string conId = data.Substring(i+4, data.Length -  (i+4));
+           // Connection.Send()
+            return Connection.Send(conId, data);
+        }
+
+     
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(MyHub.Startup))]
+
+namespace MyHub
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR<OneToOne>("/onetoone");
+            app.MapSignalR();
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
